09d331750ddf6774f20ea2f5c08956940ea2c130
Cyril Menard <c.menard@pro.neo-soft.fr>
2018-02-02 17:37:02 +0100

    tp heroes part 3

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
+import { HEROES } from '../heroes-mock';
 
 @Component({
   selector: 'app-heroes',
@@ -8,12 +9,11 @@ import { Hero } from '../hero';
 })
 export class HeroesComponent implements OnInit {
 
+ 	heroes=HEROES;
+
   constructor() { }
 
-  hero : Hero{
-  	id: 1,
-  	name: 'winstorm'
-  }
+ 
 
   ngOnInit() {
   }
